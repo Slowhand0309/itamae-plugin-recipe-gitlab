bd479aeac8b59f013aa2df0c0a4ba63d08a72d3e
Slowhand0309 <slowhand0309@gmail.com>
2016-07-11 00:49:14 +0900

    Update install recipe

--- lib/itamae/plugin/recipe/gitlab/install.rb
+++ lib/itamae/plugin/recipe/gitlab/install.rb
@@ -1,5 +1,31 @@
 # encoding: utf-8
 
+PACKAGE_SH_URL = "https://packages.gitlab.com/install/repositories/gitlab/gitlab-ce/script.rpm.sh"
 
 # Install the necessary dependencies.
 include_recipe 'gitlab::dependency'
+
+# Start postfix service.
+service "postfix" do
+  user "root"
+  action [:enable, :start]
+end
+
+execute "set firewall" do
+  user "root"
+  command "lokkit -s http -s ssh"
+end
+
+execute "add package" do
+  command "curl -sS #{PACKAGE_SH_URL} | sudo bash"
+end
+
+package 'gitlab-ce' do
+  user "root"
+  action :install
+end
+
+execute "reconfigure" do
+  user "root"
+  command "gitlab-ctl reconfigure"
+end
